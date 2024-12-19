a735e2f31c362aa6e83fb56585f83a433c221549
MauriLopezLlorenty <111530147+MauriLopezLlorenty@users.noreply.github.com>
2024-12-19 20:37:38 -0300

    pequeño cambio en la vista
    Se agregan columnas, no filas cada vez que hay un nuevo producto

--- laravel/proyecto_integrador/resources/views/productos/productos.blade.php
+++ laravel/proyecto_integrador/resources/views/productos/productos.blade.php
@@ -6,19 +6,14 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 <link rel="stylesheet" type="text/css" href="css/header.css">
 <link href="../../css/styles.css" rel="stylesheet" />
-
 <title>Document</title>
 </head>
 @include('header')
 <p>PRODUCTOS QUE TENEMOS </p>
-@foreach($productos as $producto)
-    <a href="productos/{{$producto->id_producto}}"><p>Nombre: {{$producto->nombre_producto}}</p></a>
-@endforeach
-
 <section class="py-5">
     <div class="container px-4 px-lg-5 mt-5">
-        @foreach($productos as $producto)
             <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                @foreach($productos as $producto)
                 <div class="col mb-5">
                     <div class="card h-100">
                         <!-- Product image-->
@@ -28,18 +23,16 @@
                             <div class="text-center">
                                 <!-- Product name-->
                                 <h5 class="fw-bolder"> Nombre: {{$producto->nombre_producto}}</h5>
-
                                 <p> Precio:{{$producto->precio_producto}} </p>
                             </div>
                         </div>
-
                         <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
                             <div class="text-center"><a class="btn btn-outline-dark  mt-auto" href="#">Comprar</a></div>
                         </div>
                     </div>
                 </div>
+                @endforeach
             </div>
-        @endforeach
     </div>
 </section>
 <!-- Bootstrap core JS-->
