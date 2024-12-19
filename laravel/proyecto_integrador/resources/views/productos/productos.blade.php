da4a2334b8bf53815dd66ce247a75fd5db1562fb
MauriLopezLlorenty <111530147+MauriLopezLlorenty@users.noreply.github.com>
2024-12-19 20:18:37 -0300

    Vista producos

--- laravel/proyecto_integrador/resources/views/productos/productos.blade.php
+++ laravel/proyecto_integrador/resources/views/productos/productos.blade.php
@@ -1,10 +1,49 @@
-@include('header')
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+<link rel="stylesheet" type="text/css" href="css/header.css">
+<link href="../../css/styles.css" rel="stylesheet" />
 
-<p style="color: black;">PRODUCTOS QUE TENEMOS</p>
+<title>Document</title>
+</head>
+@include('header')
+<p>PRODUCTOS QUE TENEMOS </p>
 @foreach($productos as $producto)
-    <a href="productos/{{$producto->id_producto}}" style="color: black;">
-        <p style="color: black;">Nombre: {{$producto->nombre_producto}}</p>
-    </a>
+    <a href="productos/{{$producto->id_producto}}"><p>Nombre: {{$producto->nombre_producto}}</p></a>
 @endforeach
 
-@include('footer')
+<section class="py-5">
+    <div class="container px-4 px-lg-5 mt-5">
+        @foreach($productos as $producto)
+            <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                <div class="col mb-5">
+                    <div class="card h-100">
+                        <!-- Product image-->
+                        <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                        <!-- Product details-->
+                        <div class="card-body p-4">
+                            <div class="text-center">
+                                <!-- Product name-->
+                                <h5 class="fw-bolder"> Nombre: {{$producto->nombre_producto}}</h5>
+
+                                <p> Precio:{{$producto->precio_producto}} </p>
+                            </div>
+                        </div>
+
+                        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                            <div class="text-center"><a class="btn btn-outline-dark  mt-auto" href="#">Comprar</a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+</section>
+<!-- Bootstrap core JS-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Core theme JS-->
+<script src="../../js/scripts.js"></script>
+</html>
