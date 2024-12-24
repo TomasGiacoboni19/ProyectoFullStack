5c386fa5e91d8905f7f3fc77d99879fc8cd964a9
TomasGiacoboni <tgiacoboni@frba.utn.edu.ar>
2024-12-23 22:41:44 -0300

    Corrijo header y footer

--- laravel/proyecto_integrador/resources/views/footer.blade.php
+++ laravel/proyecto_integrador/resources/views/footer.blade.php
@@ -1,39 +1,40 @@
-<footer>
-    <div class="justify-content-center container-fluid">
-        <div class="row gap-4 justify-content-center">
-            <div class="col-auto">
-                <h3>Ayuda</h3>
-                <ul>
-                    <li><a href="">Mi cuenta</a></li>
-                    <li><a href="">Pagos y facturas</a></li>
-                    <li><a href="">Opciones para regalo</a></li>
-                    <li><a href="">Articulos para tallas</a></li>
-                    <li><a href="">Formas de pago</a></li>
+<footer class="bg-dark text-white py-4">
+    <div class="container">
+        <div class="row text-center text-md-start">
+
+            <div class="col-md-4 mb-4">
+                <h3 class="text-uppercase text-success">MarcaDeRopa</h3>
+                <ul class="list-unstyled">
+                    <li><i class="bi bi-geo-alt"></i> 123 Calle Ejemplo, Ciudad 10660</li>
+                    <li><i class="bi bi-telephone"></i> 010-020-0340</li>
+                    <li><i class="bi bi-envelope"></i> info@marca.com</li>
                 </ul>
             </div>
-            <div class="col-auto">
-                <h3>Seguinos</h3>
-                <ul>
-                    <li><a href="">X</a></li>
-                    <li><a href="">Youtube</a></li>
-                    <li><a href="">Instagram</a></li>
-                    <li><a href="">Facebook</a></li>
-                    <li><a href="">Pinterest</a></li>
-                    <li><a href="">TikTok</a></li>
+
+            <div class="col-md-4 mb-4">
+                <h3 class="text-uppercase">Ayuda</h3>
+                <ul class="list-unstyled">
+                    <li><a href="" class="text-white">Mi cuenta</a></li>
+                    <li><a href="" class="text-white">Pagos y facturas</a></li>
+                    <li><a href="" class="text-white">Opciones para regalo</a></li>
+                    <li><a href="" class="text-white">Artículos para tallas</a></li>
+                    <li><a href="" class="text-white">Formas de pago</a></li>
                 </ul>
             </div>
-            <div class="col-auto">
-                <h3>Políticas</h3>
-                <ul>
-                    <li><a href="">Política de Privacidad</a></li>
-                    <li><a href="">Condiciones de Compra</a></li>
-                    <li><a href="">Condiciones de Tarjeta de Regalo</a></li>
-                    <li><a href="">Botón de Arrepentimiento</a></li>
-                    <li><a href="">Configuración de Cookies</a></li>
+
+            <div class="col-md-4 mb-4">
+                <h3 class="text-uppercase">Políticas</h3>
+                <ul class="list-unstyled">
+                    <li><a href="" class="text-white">Política de Privacidad</a></li>
+                    <li><a href="" class="text-white">Condiciones de Compra</a></li>
+                    <li><a href="" class="text-white">Condiciones de Tarjeta de Regalo</a></li>
+                    <li><a href="" class="text-white">Botón de Arrepentimiento</a></li>
+                    <li><a href="" class="text-white">Configuración de Cookies</a></li>
                 </ul>
             </div>
         </div>
+        <div class="text-center mt-3">
+            <p class="mb-0">MarcaDeRopa©2024. Todos los derechos reservados.</p>
+        </div>
     </div>
-    <p>MarcaDeRopa©2024. Todos los derechos reservados. </p>
 </footer>
-</body>
--- laravel/proyecto_integrador/resources/views/header.blade.php
+++ laravel/proyecto_integrador/resources/views/header.blade.php
@@ -6,55 +6,76 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="{{ asset('css/header.css') }}">
-    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
+    <title>E-commerce</title>
 </head>
 <body>
-    <header>
-        <div class="ofertas-deslizando">
-            {{-- algo --}}
-        </div>
-        <div class="container-fluid bloque-login">
-            <div class="row align-items-center">
-                <div class="col-4 d-flex align-items-center gap-3">
-                    <a href="/">
-                        <img src="fotos/logo.png" alt="logo">
-                    </a>
-                    <a href="/categorias">CATEGORIAS</a>
-                    <a href="/productos">PRODUCTOS</a>
-                    <a href="/nosotros">¿QUIENES SOMOS?</a>
-                    <a href="/locales">LOCALES</a>
-                </div>
-                <div class="col-4">
-                    <span class="input-group-text bg-white icono">
-                        <i class="bi bi-search"></i>
-                        <input type="email" name="email" id="email" autocomplete="off" placeholder="Buscar">
-                    </span>
-                </div>
-                <div class="col-4 cuenta">
-                    @auth
-                        <div class="iniciar-sesion">
-                            <a href="/clientes/{{ auth()->user()->id_cliente }}">
-                                <i class="bi bi-person-circle"></i>
-                            </a>
-                            <a href="/logout">
-                                <button class="btn btn-outline-light">Cerrar Sesion</button>
-                            </a>
-                        </div>
-                        <div class="carrito">
-                            <i class="bi bi-cart"></i>
-                            <p style="margin: 0">0</p>
-                        </div>
-                    @else
-                    <div class="iniciar-sesion">
-                        <a href="/login">
-                            <button class="btn btn-outline-light">Iniciar sesión</button>
-                        </a>
-                    </div>
-                    @endauth
+<header>
 
+    <div class="ofertas-deslizando text-center py-2">
+        <p class="mb-0">¡Aprovecha nuestras ofertas por tiempo limitado!</p>
+    </div>
+
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+
+            <a class="navbar-brand" href="/">
+                <img src="fotos/logo.png" alt="logo" class="logo">
+            </a>
+
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+
+            <div class="collapse navbar-collapse" id="navbarContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/categorias">Categorías</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/productos">Productos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/nosotros">¿Quiénes Somos?</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/locales">Locales</a>
+                    </li>
+                </ul>
+
+
+                <form class="d-flex me-3">
+                    <div class="input-group">
+                        <span class="input-group-text bg-white"><i class="bi bi-search"></i></span>
+                        <input class="form-control" type="text" placeholder="Buscar" aria-label="Buscar">
+                    </div>
+                </form>
+
+
+                <div class="d-flex align-items-center">
+                    @auth
+                        <a href="/clientes/{{ auth()->user()->id_cliente }}" class="btn btn-outline-light me-2">
+                            <i class="bi bi-person-circle"></i>
+                        </a>
+                        <a href="/logout" class="btn btn-outline-light me-2">Cerrar Sesión</a>
+                    @else
+                        <a href="/login" class="btn btn-outline-light me-2">Iniciar Sesión</a>
+                    @endauth
+                    <div class="position-relative">
+                        <a href="/carrito" class="btn btn-outline-light">
+                            <i class="bi bi-cart"></i>
+                        </a>
+                        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">0</span>
+                    </div>
                 </div>
             </div>
         </div>
-    </header>
+    </nav>
+</header>
 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ozLdGxQm9blE9F2ATGeZK3l+AcMYFZPNa49s5vCEx8V/Jgrf7PO/8xj9zLWoI8+q" crossorigin="anonymous"></script>
+</body>
+</html>
