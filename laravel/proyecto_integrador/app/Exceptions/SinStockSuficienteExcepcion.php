8ddda50150735655224cdfc463e82b66488fc117
Lucassiturrioz <lucasiturrioz212@gmail.com>
2024-12-29 20:58:11 -0300

    Eliminar ProductoController.php

---
+++ laravel/proyecto_integrador/app/Exceptions/SinStockSuficienteExcepcion.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+class SinStockSuficienteExcepcion
+{
+
+    /**
+     * @param string $string
+     */
+    public function __construct(string $string)
+    {
+    }
+}
