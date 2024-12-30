2c7133fff0bdd33a82ff64fa243998ffac120be3
Bruno Rodriguez <brrodriguez@frba.utn.edu.ar>
2024-12-30 10:38:53 -0300

    Arreglos menores

--- laravel/proyecto_integrador/resources/views/clientes/editar_cliente.blade.php
+++ laravel/proyecto_integrador/resources/views/clientes/editar_cliente.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/header.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/footer.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/perfilUsuario.css')}}">
@@ -14,7 +15,10 @@
 @include('header', ['carrito' => $carrito])
 
 <section id="editar-perfil" class="container mt-4">
-    <h3>Editar Información Personal</h3>
+    <div class="encabezado">
+        <h3>Editar Información Personal</h3>
+        <img src="{{ asset('fotos/datosPerfil.png') }}" alt="fotoEditar" class="fotoPequenia">
+    </div>
     <hr>
     <form action="/clientes/{{ $cliente->id_cliente }}/actualizar" method="POST">
         @csrf
